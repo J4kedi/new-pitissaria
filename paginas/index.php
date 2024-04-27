82f4473925d6a3276136fd5824fd58a1a234db10
Kauan Pardini Augsuto <kauanpardini@gmail.com>
2024-04-27 16:29:38 -0300

    Adicionado pastas do projeto, afim de ter uma padronizacao de como será até o final do desenvolvimento

---
+++ paginas/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Style/padrao.css">
+    <title>Pitissaria</title>
+</head>
+<body>
+    <?PHP include('geral/header.php');?>
+
+    <main class="container"> 
+        <h1>Bem vindo à Pitissaria</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae eligendi aperiam temporibus id. Dicta, rem quis similique nobis nam inventore suscipit voluptate quos eveniet architecto libero cumque provident itaque tempora?</p>
+    </main>
+
+    <?PHP include('geral/footer.php');?>
+</body>
+</html>
