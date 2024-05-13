212582060c61f099bd516451d4e93a9f5c0aa6f1
Kauan Pardini <kauan.pardini@pucpr.edu.br>
2024-05-13 09:08:23 -0300

    Banco de dados corrigido

--- database/script.sql
+++ database/script.sql
@@ -1,9 +1,9 @@
 -- Derrubando banco de dados
-DROP DATABASE IF EXISTS pizzaria_db;
+DROP DATABASE IF EXISTS pitissariadb;
 
 -- Criação do banco de dados
-CREATE DATABASE IF NOT EXISTS pizzaria_db;
-USE pizzaria_db;
+CREATE DATABASE IF NOT EXISTS pitissariadb;
+USE pitissariadb;
 
 -- Tabela de ingredientes
 CREATE TABLE IF NOT EXISTS ingredientes (
@@ -12,6 +12,19 @@ CREATE TABLE IF NOT EXISTS ingredientes (
     preco DECIMAL(8,2) NOT NULL,
     data_entrada DATE NOT NULL,
     data_validade DATE NOT NULL
+);
+
+-- Tabela de usuários
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(32) NOT NULL,
+    cpf VARCHAR(20) NOT NULL UNIQUE,
+    tipo_usuario ENUM('cliente', 'pizzaiolo', 'gerente') DEFAULT 'cliente',
+    data_nascimento DATE,
+    celular VARCHAR(20),
+    username VARCHAR(50)
 );
 
 -- Tabela de endereços
@@ -23,23 +36,17 @@ CREATE TABLE IF NOT EXISTS enderecos (
     cidade VARCHAR(50) NOT NULL
 );
 
--- Tabela de usuários
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(32) NOT NULL,
-    cpf VARCHAR(20) NOT NULL UNIQUE,
-    tipo_usuario ENUM('cliente', 'pizzaiolo', 'gerente') DEFAULT 'cliente',
+-- Tabela intermediária para a relação muitos para muitos entre usuários e endereços
+CREATE TABLE IF NOT EXISTS usuario_endereco (
+    usuario_id INT,
     endereco_id INT,
-    data_nascimento DATE,
-    celular VARCHAR(20),
-    username VARCHAR(50),
-    FOREIGN KEY (endereco_id) REFERENCES enderecos(id) -- Referencia a coluna id da tabela enderecos
+    PRIMARY KEY (usuario_id, endereco_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (endereco_id) REFERENCES enderecos(id)
 );
 
--- Tabela de pizzas personalizadas
-CREATE TABLE IF NOT EXISTS pizzas_personalizadas (
+-- Tabela de pizzas
+CREATE TABLE IF NOT EXISTS pizzas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     descricao TEXT,
@@ -48,13 +55,13 @@ CREATE TABLE IF NOT EXISTS pizzas_personalizadas (
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id) -- Restrição de chave estrangeira com a tabela usuarios
 );
 
--- Tabela de ingredientes das pizzas personalizadas
-CREATE TABLE IF NOT EXISTS ingredientes_pizzas_personalizadas (
+-- Tabela de ingredientes das pizzas
+CREATE TABLE IF NOT EXISTS ingredientes_pizzas (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    id_pizza_personalizada INT,
+    id_pizza INT,
     id_ingrediente INT,
     quantidade INT,
-    FOREIGN KEY (id_pizza_personalizada) REFERENCES pizzas_personalizadas(id), -- Restrição de chave estrangeira com a tabela pizzas_personalizadas
+    FOREIGN KEY (id_pizza) REFERENCES pizzas(id), -- Restrição de chave estrangeira com a tabela pizzas
     FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id) -- Restrição de chave estrangeira com a tabela ingredientes
 );
 
@@ -69,15 +76,15 @@ CREATE TABLE IF NOT EXISTS pedidos (
     FOREIGN KEY (endereco_entrega_id) REFERENCES enderecos(id)
 );
 
--- Tabela de itens do pedido para pizzas personalizadas
-CREATE TABLE IF NOT EXISTS itens_pedido_personalizadas (
+-- Tabela de itens do pedido para pizzas
+CREATE TABLE IF NOT EXISTS itens_pedido (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_pedido INT,
-    id_pizza_personalizada INT,
+    id_pizza INT,
     quantidade INT,
     preco_unitario DECIMAL(8,2) NOT NULL,
     FOREIGN KEY (id_pedido) REFERENCES pedidos(id),
-    FOREIGN KEY (id_pizza_personalizada) REFERENCES pizzas_personalizadas(id)
+    FOREIGN KEY (id_pizza) REFERENCES pizzas(id)
 );
 
 -- Inserção de dados de exemplo
@@ -91,9 +98,8 @@ INSERT INTO ingredientes (nome, preco, data_entrada, data_validade) VALUES
     ('Azeitonas', 1.50, '2022-04-01', '2022-10-01');
 
 select * from usuarios;
-delete from usuarios where id = 3;
 
-select * from pizzas_personalizadas;
-select * from ingredientes_pizzas_personalizadas;
+select * from pizzas;
+select * from ingredientes_pizzas;
 -- Select para a tabela de pedidos
 SELECT * FROM pedidos;
